85bae0f0fb9a14f2c05822f9eb5ec375923ac4cf
NovobRom <romannovobranets@gmail.com>
2026-05-07 13:03:20 +0300

    Fix lint: escape quotes in page.tsx, remove unused SectionNav import

--- app/shops/page.tsx
+++ app/shops/page.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next'
 import { getPrevNext } from '@/lib/navigation'
-import { SectionNav } from '@/components/SectionNav'
 import { VisitTracker } from '@/components/VisitTracker'
 import { ShopCard } from '@/components/ShopCard'
 
